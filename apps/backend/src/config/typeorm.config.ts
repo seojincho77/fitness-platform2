db71e23cbdfc0b5119895acf7ae503a1f22fc51d
seojincho77 <choseo0215@naver.com>
2025-04-11 19:55:11 +0900

    Update typeorm.config.ts

--- apps/backend/src/config/typeorm.config.ts
+++ apps/backend/src/config/typeorm.config.ts
@@ -1,4 +1,11 @@
 import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
 import { DataSource } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { BodyRecord } from '../body-records/entities/body-record.entity';
@@ -6,13 +13,14 @@ import { Exercise } from '../exercises/entities/exercise.entity';
 import { Post } from '../posts/entities/post.entity';
 import { Routine } from '../routines/entities/routine.entity';
 
-
-
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  entities: [  User,BodyRecord,Exercise,Post,Routine,],
+  host: 'postgres',          
+  port: 5432,
+  username: 'user',          
+  password: 'pass',
+  database: 'fitness_db',
+  entities: [User, BodyRecord, Exercise, Post, Routine],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
 });
