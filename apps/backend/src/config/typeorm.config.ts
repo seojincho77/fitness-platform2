d9f67a5f636559b818d5a1be34b70e6f90a0d606
KDKang <kdkang97425@gmail.com>
2025-04-16 15:24:57 +0900

    conflict

--- apps/backend/src/config/typeorm.config.ts
+++ apps/backend/src/config/typeorm.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
@@ -25,31 +24,3 @@ export const AppDataSource = new DataSource({
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
 });
-=======
-import 'reflect-metadata';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({
-  path: path.resolve(__dirname, '../../.env'),
-});
-
-import { DataSource } from 'typeorm';
-import { User } from '../users/entities/user.entity';
-import { BodyRecord } from '../body-records/entities/body-record.entity';
-import { Exercise } from '../exercises/entities/exercise.entity';
-import { Post } from '../posts/entities/post.entity';
-import { Routine } from '../routines/entities/routine.entity';
-
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [User, BodyRecord, Exercise, Post, Routine],
-  migrations: ['src/migrations/*.ts'],
-  synchronize: false,
-});
->>>>>>> 29a2bb83de5f7d355780e2ee41bf437e0970f6cf
