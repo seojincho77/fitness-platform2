5cc6fd1fd33a2e67ffd353d1a331e7495081295a
KDKang <kdkang97425@gmail.com>
2025-04-16 15:13:57 +0900

    migration

---
+++ apps/backend/data-source.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/src/**/*.entity.{ts,js}'],
+  migrations: [__dirname + '/migrations/*.{ts,js}'],
+  synchronize: false,
+};
+
+export const AppDataSource = new DataSource(dataSourceOptions);
---
+++ apps/backend/src/config/typeorm.config.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
+import { DataSource } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { BodyRecord } from '../body-records/entities/body-record.entity';
+import { Exercise } from '../exercises/entities/exercise.entity';
+import { Post } from '../posts/entities/post.entity';
+import { Routine } from '../routines/entities/routine.entity';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [User, BodyRecord, Exercise, Post, Routine],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false,
+});
