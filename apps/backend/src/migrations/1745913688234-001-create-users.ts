f6e2f4d81d647cac4e80868d9aad3b034506fc62
KDKang <kdkang97425@gmail.com>
2025-04-30 15:09:44 +0900

    migration minor change

---
+++ apps/backend/src/migrations/1745913688234-001-create-users.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class 001CreateUsers1745913688234 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
